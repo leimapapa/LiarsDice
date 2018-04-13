f8b8a55f08cecba0ce3a2511186eb1e8bff13101
leimapapa <36284756+leimapapa@users.noreply.github.com>
2018-04-12 23:44:46 -0600

    Create liarsDice.js
    contains functions liarsDice() and loseDie()

---
+++ liarsDice.js
@@ -0,0 +1,186 @@
+function liarsDice(){
+
+var resetGameBoard = document.getElementById("liarsDiceBoard");
+var bonesRoller = document.getElementById("bonesRoller");
+
+if ($("#loser").hassClass("bigger")){
+
+  resetGameBoard.innerHTML = "INSERT GAME BOARD DIV HERE";
+  bonesRoller.innerHTML = "Roll Them Bones<br/><i class="glyphicon glyphicon-repeat"></i>";
+  
+  }
+  
+var randomNumber1 = Math.round(Math.random() * 5) + 1;
+var randomNumber2 = Math.round(Math.random() * 5) + 1;
+var randomNumber3 = Math.round(Math.random() * 5) + 1;
+var randomNumber4 = Math.round(Math.random() * 5) + 1;
+var randomNumber5 = Math.round(Math.random() * 5) + 1;
+var randomNumber6 = Math.round(Math.random() * 5) + 1;
+
+var liarsDice1 = document.getElementById("liarsDice1");
+var liarsDice2 = document.getElementById("liarsDice2");
+var liarsDice3 = document.getElementById("liarsDice3");
+var liarsDice4 = document.getElementById("liarsDice4");
+var liarsDice5 = document.getElementById("liarsDice5");
+var liarsDice6 = document.getElementById("liarsDice6");
+
+switch(randomNumber1) {
+  case 1:
+  liarsDice1.innerHTML = "<img src='dice/die1.png'/>";
+  break;
+  case 2:
+  liarsDice1.innerHTML = "<img src='dice/die2.png'/>";
+  break;
+  case 3:
+  liarsDice1.innerHTML = "<img src='dice/die3.png'/>";
+  break;
+  case 4:
+  liarsDice1.innerHTML = "<img src='dice/die4.png'/>";
+  break;
+  case 5:
+  liarsDice1.innerHTML = "<img src='dice/die5.png'/>";
+  break;
+  case 6:
+  liarsDice1.innerHTML = "<img src='dice/die6.png'/>";
+  break;
+}
+switch(randomNumber2) {
+  case 1:
+  liarsDice2.innerHTML = "<img src='dice/die1.png'/>";
+  break;
+  case 2:
+  liarsDice2.innerHTML = "<img src='dice/die2.png'/>";
+  break;
+  case 3:
+  liarsDice2.innerHTML = "<img src='dice/die3.png'/>";
+  break;
+  case 4:
+  liarsDice2.innerHTML = "<img src='dice/die4.png'/>";
+  break;
+  case 5:
+  liarsDice2.innerHTML = "<img src='dice/die5.png'/>";
+  break;
+  case 6:
+  liarsDice2.innerHTML = "<img src='dice/die6.png'/>";
+  break;
+}
+switch(randomNumber3) {
+  case 1:
+  liarsDice3.innerHTML = "<img src='dice/die1.png'/>";
+  break;
+  case 2:
+  liarsDice3.innerHTML = "<img src='dice/die2.png'/>";
+  break;
+  case 3:
+  liarsDice3.innerHTML = "<img src='dice/die3.png'/>";
+  break;
+  case 4:
+  liarsDice3.innerHTML = "<img src='dice/die4.png'/>";
+  break;
+  case 5:
+  liarsDice3.innerHTML = "<img src='dice/die5.png'/>";
+  break;
+  case 6:
+  liarsDice3.innerHTML = "<img src='dice/die6.png'/>";
+  break;
+}
+switch(randomNumber4) {
+  case 1:
+  liarsDice4.innerHTML = "<img src='dice/die1.png'/>";
+  break;
+  case 2:
+  liarsDice4.innerHTML = "<img src='dice/die2.png'/>";
+  break;
+  case 3:
+  liarsDice4.innerHTML = "<img src='dice/die3.png'/>";
+  break;
+  case 4:
+  liarsDice4.innerHTML = "<img src='dice/die4.png'/>";
+  break;
+  case 5:
+  liarsDice4.innerHTML = "<img src='dice/die5.png'/>";
+  break;
+  case 6:
+  liarsDice4.innerHTML = "<img src='dice/die6.png'/>";
+  break;
+}
+switch(randomNumber5) {
+  case 1:
+  liarsDice5.innerHTML = "<img src='dice/die1.png'/>";
+  break;
+  case 2:
+  liarsDice5.innerHTML = "<img src='dice/die2.png'/>";
+  break;
+  case 3:
+  liarsDice5.innerHTML = "<img src='dice/die3.png'/>";
+  break;
+  case 4:
+  liarsDice5.innerHTML = "<img src='dice/die4.png'/>";
+  break;
+  case 5:
+  liarsDice5.innerHTML = "<img src='dice/die5.png'/>";
+  break;
+  case 6:
+  liarsDice5.innerHTML = "<img src='dice/die6.png'/>";
+  break;
+}
+switch(randomNumber6) {
+  case 1:
+  liarsDice6.innerHTML = "<img src='dice/die1.png'/>";
+  break;
+  case 2:
+  liarsDice6.innerHTML = "<img src='dice/die2.png'/>";
+  break;
+  case 3:
+  liarsDice6.innerHTML = "<img src='dice/die3.png'/>";
+  break;
+  case 4:
+  liarsDice6.innerHTML = "<img src='dice/die4.png'/>";
+  break;
+  case 5:
+  liarsDice6.innerHTML = "<img src='dice/die5.png'/>";
+  break;
+  case 6:
+  liarsDice6.innerHTML = "<img src='dice/die6.png'/>";
+  break;
+}
+
+//********************
+
+
+function loseDie(){
+
+  var gameBoard = document.getElementById("liarsDiceBoard");
+  var bonesRoller = document.getElementById("bonesRoller");
+  
+  if ($("#liar6").hasClass("inTheGame")){
+    $("#liar6").animate({opacity:"0"},"slow");
+    $("#liar6").removeClass("inTheGame");
+    $("#liar5").addClass("nextUp");
+  } else if ($("#liar5").hasClass("inTheGame") && $("#liar5").hasClass("nextUp")){
+    $("#liar5").animate({opacity:"0"},"slow");
+    $("#liar5").removeClass("inTheGame");
+    $("#liar5").removeClass("inTheGame");
+    $("#liar4").addClass("nextUp");
+  } else if ($("#liar4").hasClass("inTheGame") && $("#liar4").hasClass("nextUp")){
+    $("#liar4").animate({opacity:"0"},"slow");
+    $("#liar4").removeClass("inTheGame");
+    $("#liar4").removeClass("inTheGame");
+    $("#liar3").addClass("nextUp");
+  } else if ($("#liar3").hasClass("inTheGame") && $("#liar3").hasClass("nextUp")){
+    $("#liar3").animate({opacity:"0"},"slow");
+    $("#liar3").removeClass("inTheGame");
+    $("#liar3").removeClass("inTheGame");
+    $("#liar2").addClass("nextUp");
+  } else if ($("#liar2").hasClass("inTheGame") && $("#liar2").hasClass("nextUp")){
+    $("#liar2").animate({opacity:"0"},"slow");
+    $("#liar2").removeClass("inTheGame");
+    $("#liar2").removeClass("inTheGame");
+    $("#liar1").addClass("nextUp");
+  } else if ($("#liar1").hasClass("inTheGame") && $("#liar1").hasClass("nextUp")){
+    $("#liar1").animate({opacity:"0"},"slow");
+    gameBoard.innerHTML = "<p id='loser' class='bigger'>Bootstrap Bill, you're a liar, and you will spend an eternity on this ship!</p>";
+    bonesRoller.innerHTML = "New Game";
+  }
+  
+}
